84164a28ef3fcf36d68960e2ab09aaa312120b9f
woorea <luis@woorea.es>
2013-03-09 13:00:06 +0100

    bf

--- billingstack-api/grails-app/controllers/com/billingstack/BillingStackController.groovy
+++ billingstack-api/grails-app/controllers/com/billingstack/BillingStackController.groovy
@@ -7,32 +7,32 @@ class BillingStackController {
     def create() {
       try {
         def json = request.JSON
-				println json
+        println json
         def user
         if(json.token) {
           //get merchant.id
         } else if(json.api_key && json.api_secret) {
           user = User.findByApiKeyAndApiSecret(json.api_key, json.api_secret)
         } else {
-					user = User.findByUsername(json.username)
+          user = User.findByUsername(json.username)
         }
         if(user) {
-					if(!user.password.equals(json.password)) {
-						throw new RuntimeException("wrong.password")
-					}
-					def access = [
+          if(!user.password.equals(json.password)) {
+            throw new RuntimeException("wrong.password")
+          }
+          def access = [
             token : (UUID.randomUUID() as String).replaceAll('-',""),
-						endpoint : createLink(controller : 'application', absolute : true) as String
+            endpoint : createLink(controller : 'application', absolute : true) as String
           ]
-					if(json.account) {
-						def account = Account.findByIdOrName(json.account, json.account)
-	          def ur = AccountUserRole.findAllByUserAndAccount(user, account)
-						access.account_id = account.id
-						access.account_name = account.name
-						access.account_roles = ur.collect { it.role.name }
-						
-					}
-					render(text: access as JSON, contentType: 'application/json', encoding:"UTF-8")
+          if(json.account) {
+            def account = Account.findByIdOrName(json.account, json.account)
+            def ur = AccountUserRole.findAllByUserAndAccount(user, account)
+            access.account_id = account.id
+            access.account_name = account.name
+            access.account_roles = ur.collect { it.role.name }
+            
+          }
+          render(text: access as JSON, contentType: 'application/json', encoding:"UTF-8")
           //def tokens = hazelcastService.map("tokens")
           //tokens.put(access.token.id, access.token)
           
